9deb50bf085aaed2eb7de1a90cfcbb7ca6b765ae
lilixu3 <lilixu333@gmail.com>
2025-12-12 20:43:54 +0800

    更新 systemsettings.js

--- danmu_api/ui/js/systemsettings.js
+++ danmu_api/ui/js/systemsettings.js
@@ -645,11 +645,13 @@ function renderEnvList() {
                             <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
                         </svg>
+                        <span>编辑</span>
                     </button>
                     <button class="btn btn-danger btn-sm" onclick="deleteEnv(\${index})" title="删除">
                         <svg class="btn-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                             <path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
                         </svg>
+                        <span>删除</span>
                     </button>
                 </div>
             </div>
